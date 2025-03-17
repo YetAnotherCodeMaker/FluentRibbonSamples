8c57ca89184814f1b17c7c04a7b86c86a14dbe72
Ramon Miguel <rmiguel@astrocast.com>
2025-03-17 17:32:03 +0100

    Add V11RibbonToggleButtonIssue showcase

---
+++ V11RibbonToggleButtonIssue/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace RibbonToggleButtonsShowcase;
+
+public enum Options { A, B, C, D }
+
+public partial class MainWindow
+{
+    [STAThread]
+    public static void Main() =>
+        new Application().Run(new MainWindow());
+
+    public MainWindow() =>
+        InitializeComponent();
+}
